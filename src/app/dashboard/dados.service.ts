834659959f50faf5e37c55ce3225c6124af1d8f4
= <=>
2021-11-04 12:03:37 -0300

    implementados dados para testes

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DadosService {
 
+  readonly dados = [
+    ['Janeiro',32],
+    ['Fevereiro',41],
+    ['Março',39],
+    ['Abril',24],
+    ['Maio',35],
+    ['Junho',49],
+    ['Julho',28],
+    ['Agosto',46],
+    ['Setembro',65],
+    ['Outubro',89],
+    ['Novembro',85],
+    ['Dezembro',75],
+
+  ]
+
   constructor() { }
+
+  /**
+   * Retorna um observable contendo os dados a serem exibidos no gráfico.
+   * @returns Observable<any>
+   */
+  obterDados(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.dados);
+      observable.complete();
+    });
+  }
 }
