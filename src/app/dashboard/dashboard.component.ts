7e60e6f51ccd13e32a8e3b97c43b6855c2020f4a
= <=>
2021-11-04 14:06:47 -0300

    implementada a apresentação dos gráficos.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DadosService } from './dados.service';
+
+declare var google: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,146 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  private dados: any;
+  
+  constructor(private dadosService: DadosService) { }
 
   ngOnInit(): void {
+    this.dadosService.obterDados().subscribe(
+      dados => {
+        this.dados = dados;
+        this.init()
+      }
+    )
+  }
+  /**
+   * Inicializa a API de gráficos com delay de 1 segundo,
+   * o que permite a integração da API com o Angular.
+   * 
+   * @return void
+   */
+  init(): void{
+    if(typeof(google) !== 'undefined') {
+      google.charts.load('current',{'packages':['corechart']});
+      setTimeout(() => {
+        google.charts.setOnLoadCallback(this.exibirGraficos());
+      }, 1000)
+    }
   }
 
+  /**
+   * Método chamado assim que a API de gráficos é inicializada.
+   * Responsável por chamar os métodos geradores dos gráficos.
+   */
+  exibirGraficos(){
+    this.exibirPieChart();
+    this.exibir3dPieChart();
+    this.exibirDonutChart();
+    this.exibirBarChart();
+    this.exibirLineChart();
+    this.exibirColumnChart();
+  }
+  /**
+   * Exibe o gráfico Pie Chart
+   * 
+   * @return void
+   */
+  exibirPieChart(): void {
+    const el = document.getElementById('pie_chart');
+    const chart = new google.visualization.PieChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes());
+  }
+  
+  /**
+   * Exibe o gráfico 3d Pie Chart
+   * 
+   * @return void
+   */
+  exibir3dPieChart(): void {
+    const el = document.getElementById('3d_pie_chart');
+    const chart = new google.visualization.PieChart(el);
+    let opcoes = this.obterOpcoes();
+
+    opcoes['is3D'] = true;
+    chart.draw(this.obterDataTable(), opcoes);
+  }
+
+  /**
+   * Exibe o gráfico Donut Chart
+   * 
+   * @return void
+   */
+   exibirDonutChart(): void {
+    const el = document.getElementById('donut_chart');
+    const chart = new google.visualization.PieChart(el);
+    const opcoes = this.obterOpcoes();
+
+    opcoes['pieHole'] = 0.4;
+    chart.draw(this.obterDataTable(), opcoes);
+  }
+
+  /**
+   * Exibe o gráfico Bar Chart
+   * 
+   * @return void
+   */
+   exibirBarChart(): void {
+    const el = document.getElementById('bar_chart');
+    const chart = new google.visualization.BarChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes());
+  }
+
+  /**
+   * Exibe o gráfico Line Chart
+   * 
+   * @return void
+   */
+   exibirLineChart(): void {
+    const el = document.getElementById('line_chart');
+    const chart = new google.visualization.LineChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes());
+  }
+
+  /**
+   * Exibe o gráfico Line Chart
+   * 
+   * @return void
+   */
+   exibirColumnChart(): void {
+    const el = document.getElementById('column_chart');
+    const chart = new google.visualization.ColumnChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes());
+  }
+
+  /**
+   * Cria e retorna o objeto DataTable da API de gráficos,
+   * responsável por definir os dados do gráfico.
+   * @returns any
+   */
+  obterDataTable(): any{
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string','Mês');
+    data.addColumn('number','Quantidade');
+    data.addRows(this.dados);
+
+    return data;
+  }
+
+    /**
+     * Retorna as opções do gráfico, que incluem o título
+     * e tamanho do gráfico.
+     * @returns any
+     */
+    obterOpcoes(): any {
+      return {
+        'title': 'Quantidade de cadastros primeiro semestre',
+        'width': 400,
+        'height': 300
+      }
+    }
 }
